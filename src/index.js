729b36f2740c7b97f5f0e771bde84be7470703c0
ogel <yocorangelo18@gmail.com>
2023-12-29 10:40:47 +0800

    idk what is cors is

--- src/index.js
+++ src/index.js
@@ -33,10 +33,11 @@ app.use(session(
  }
 ));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+    if (shouldDisableCORS(req)) {
+        res.header('Access-Control-Allow-Origin', false);
+    } else {
+        next();
+    }
 });
 
 app.use(cookieParser())
