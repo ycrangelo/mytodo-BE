f8944faed81ae166d1762348987d011fd87eba62
ogel <yocorangelo18@gmail.com>
2023-12-29 11:27:00 +0800

    tite

--- src/index.js
+++ src/index.js
@@ -33,11 +33,10 @@ app.use(session(
  }
 ));
 app.use((req, res, next) => {
-   
-        res.header('Access-Control-Allow-Origin', true);
-   
-        next();
-    
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.use(cookieParser())
