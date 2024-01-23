bb40ce4bee21c7be8f1c4302367a921c51ddaf4c
ogel <yocorangelo18@gmail.com>
2024-01-23 14:15:22 +0800

    change the auth response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -55,11 +55,7 @@ router.post('/register', async (req, res) => {
       //then saving it
       await newUser.save()
       //sending a response to the client side
-      if (newUser) {
-  res.status(201).send(newUser);
-} else {
-  res.status(200).send({ message: 'Registration successful' });
-}
+      res.status(201).send(email)
     }
   } catch (error) {
     console.log(error)
