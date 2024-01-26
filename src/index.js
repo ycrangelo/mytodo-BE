be0d9de6fe4fa0fc7670187c005b9e74d552c4c7
ogel <yocorangelo18@gmail.com>
2024-01-26 18:05:36 +0800

    PLEASE HELP

--- src/index.js
+++ src/index.js
@@ -22,6 +22,16 @@ const corsOptions = {
    exposedHeaders: ['Access-Control-Allow-Origin'],
 };
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+   res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+   // Log the headers for debugging
+   console.log(res.getHeaders());
+
+   next();
+});
 
 
 //port/localHost
