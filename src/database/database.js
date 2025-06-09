4754bf2791740e7371297f569f8799eb0e68e989
ycrangelo <yocorangelo18@gmail.com>
2025-06-09 13:20:35 +0800

    update mongostore and string

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
- mongoose.connect('mongodb+srv://angeloyocoryocor:angeloyocoryocor@cluster0.p13owno.mongodb.net/?retryWrites=true&w=majority').then(() => {
+ mongoose.connect('mongodb+srv://yocorangelo13:cjHDVxnILbyOFpdY@cluster0.cxuo6j0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
  console.log('connected in db')
 }).catch((err)=>console.log(err))
